4447b7d7ca5d94969f3f596bb925bcdcc6fca504
Frederic Crozat <fcrozat@suse.com>
2014-10-20 17:23:17 +0200

    update time on watchface

--- src/main.c
+++ src/main.c
@@ -3,25 +3,49 @@
 static Window* s_main_window;
 static TextLayer* s_time_layer;
 
+
+static void update_time() {
+    time_t temp = time(NULL);
+    struct_tm *tick_time = localtime(&temp);
+
+    static char buffer[] = "00:00";
+
+    if (clock_is_24h_style() == true) {
+        strftime(&buffer, sizeof(buffer), "%H:%M", tick_time);
+    } else {
+        strftime(&buffer, sizeof(buffer), "%I:%M", tick_time);
+    }
+
+    text_layer_set_text (s_time_layer, buffer);
+}
+
+static void tick_handler(struct_tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+
 static void main_load_window(Window *window) {
 
     s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
 
     text_layer_set_background_color (s_time_layer, GColorClear);
     text_layer_set_text_color (s_time_layer, GColorBlack);
-    text_layer_set_text (s_time_layer, "00:00");
 
     text_layer_set_font (s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
     text_layer_set_text_alignment (s_time_layer, GTextAlignmentCenter);
 
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 
+    tick_timer_service_subscribe (MINUTE_UNIT, tick_handler);
+
 }
 
 static void main_unload_window(Window *window) {
 
+    tick_timer_service_unsubscribe ();
+
     text_layer_destroy (s_time_layer);
 }
+
 
 static void init() {
 
@@ -33,6 +57,8 @@ static void init() {
             });
 
     window_stack_push(s_main_window, true);
+
+    update_time();
 }
 
 static void deinit() {
